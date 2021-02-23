217dca609085090fda4bd0d0e705fd914e8db4f0
Ezra Katzman <47403054+EzraKatzman@users.noreply.github.com>
2021-02-23 09:52:20 -0500

    corrected svg color, removed animation

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -14,8 +14,8 @@ const Navbar = () => {
                 <div className='sm:hidden'>
                     <button onClick={() => setView(!isOpen)} type='button' className='block focus:outline-none'>
                         <svg className='h-6 w-6 fill-current' viewBox='0 0 24 24'>
-                            <path className={isOpen ? 'block' : 'hidden'} fillRule='evenodd' d='M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z'/>
-                            <path className={isOpen ? 'hidden' : 'block'} fillRule='evenodd' d='M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z'/>
+                            <path className={isOpen ? 'block dark:text-gray-50' : 'hidden'} fillRule='evenodd' d='M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z'/>
+                            <path className={isOpen ? 'hidden' : 'block dark:text-gray-50'} fillRule='evenodd' d='M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z'/>
                         </svg>
                     </button>
                 </div>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
                     
                     <div className='flex justify-center p-2 mt-2'>
                         <p className='text-lg p-2'>Sign up to track your high scores</p>
-                        <Link to='/signup' className='p-2 font-bold bg-tgray-900 dark:bg-tgray-50 hover:bg-tgray-700 dark:hover:bg-tgray-200 text-tgray-50 dark:text-tgray-900 rounded-full sm:animate-bounce'>Join Now</Link>
+                        <Link to='/signup' className='p-2 font-bold bg-tgray-900 dark:bg-tgray-50 hover:bg-tgray-700 dark:hover:bg-tgray-200 text-tgray-50 dark:text-tgray-900 rounded-full'>Join Now</Link>
                     </div>
                     
                 </div>
@@ -22,15 +22,15 @@ const Home = () => {
                 <div className="text-center">
                     <h2 className="font-semibold text-4xl p-4">Features</h2>
                     <div className='sm:flex sm:justify-center sm:items-center'>
-                        <div className='m-2 p-2 w-1/3'>
+                        <div className='m-2 p-2 sm:w-1/3'>
                             <h3 className="font-semibold dark:text-tgray-100">Auto-generated Math Problems</h3>
                             <p className="dark:text-tgray-300">Magic Numbers generates its own math problems, and ensures that each one as a clean solution</p>
                         </div>
-                        <div className='m-2 p-2 w-1/3'>
+                        <div className='m-2 p-2 sm:w-1/3'>
                             <h3 className="font-semibold dark:text-tgray-100">3 Difficulty Levels</h3>
                             <p className="dark:text-tgray-300">Pick between easy, medium, and hard difficulties and improve your math reflexes</p>
                         </div>
-                        <div className='m-2 p-2 w-1/3'>
+                        <div className='m-2 p-2 sm:w-1/3'>
                             <h3 className="font-semibold dark:text-tgray-100">Leaderboards</h3>
                             <p className="dark:text-tgray-300">See if you have what it takes to get your name on the list of top performers</p>
                         </div>
